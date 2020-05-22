c6aaf739ed7340e4deab13d3a667f72d6be68f1f
Administrator <635742310@qq.com>
2020-05-22 13:48:51 +0800

    update

---
+++ pages/quotation/quotation.js
@@ -0,0 +1,108 @@
+// pages/quotation/quotation.js
+const app = getApp()
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    servenImg: '',
+    items: [
+      {
+        id:'1',
+        name:"百变房铝合金窗",
+        secondName:"凤铝1.4厚铝合金窗",
+        num:"1",
+        type:['加厚型','铝合金'],
+        money:"2100"            
+      }
+    ],
+    startX: 0, //开始坐标
+    startY: 0
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.setData({
+      servenImg: app.uploadImg.url,
+    })
+
+    // var that = this;
+    // for (var i = 0; i < 10; i++) {
+    //   that.data.items.push({
+    //     content: i + " 向左滑动删除哦,向左滑动删除哦,向左滑动删除哦,向左滑动删除哦,向左滑动删除哦",
+    //     isTouchMove: false //默认隐藏删除
+    //   })
+    // }
+    // this.setData({
+    //   items: this.data.items
+    // });
+  },
+
+  //开始触摸时 重置所有删除
+  touchstart: function (e) {
+    this.data.items.forEach(function (v, i) {
+      if (v.isTouchMove) //只操作为true的
+        v.isTouchMove = false;
+    })
+    this.setData({
+      startX: e.changedTouches[0].clientX,
+      startY: e.changedTouches[0].clientY,
+      items: this.data.items
+    })
+  },
+
+  //滑动事件处理
+  touchmove: function (e) {
+    var that = this,
+      index = e.currentTarget.dataset.index, //当前索引
+      startX = that.data.startX, //开始X坐标
+      startY = that.data.startY, //开始Y坐标
+      touchMoveX = e.changedTouches[0].clientX, //滑动变化坐标
+      touchMoveY = e.changedTouches[0].clientY, //滑动变化坐标
+
+      //获取滑动角度
+      angle = that.angle({
+        X: startX,
+        Y: startY
+      }, {
+        X: touchMoveX,
+        Y: touchMoveY
+      });
+    that.data.items.forEach(function (v, i) {
+      v.isTouchMove = false
+      //滑动超过30度角 return
+      if (Math.abs(angle) > 30) return;
+      if (i == index) {
+        if (touchMoveX > startX) //右滑
+          v.isTouchMove = false
+        else //左滑
+          v.isTouchMove = true
+      }
+    })
+    //更新数据
+    that.setData({
+      items: that.data.items
+    })
+  },
+
+
+
+  //计算滑动角度
+  angle: function (start, end) {
+    var _X = end.X - start.X,
+      _Y = end.Y - start.Y
+    //返回角度 /Math.atan()返回数字的反正切值
+    return 360 * Math.atan(_Y / _X) / (2 * Math.PI);
+  },
+
+  //删除事件
+  del: function (e) {
+    this.data.items.splice(e.currentTarget.dataset.index, 1)
+    this.setData({
+      items: this.data.items
+    })
+  }
+})
